ca00c0bc990549b77c742c724301f5a8e4872338
Kyle Grierson <griersonky@gmail.com>
2015-08-11 21:13:24 +0100

    Example of layout, Included Goal to output. Removed unneeded if statement

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ class Node:
     def __init__(self, title=None, ideas=None):
         self.title = title
 
-        if ideas:
-            for key in ideas.keys():
-                if 'ideas' in ideas[key]:
-                    LEAFS.append("{}".format(
-                        Node(ideas[key]['title'], ideas[key]['ideas'])))
-                else:
-                    LEAFS.append("\t{}".format(ideas[key]['title']))
+        for key in ideas.keys():
+            if 'ideas' in ideas[key]:
+                LEAFS.append("{}".format(
+                    Node(ideas[key]['title'], ideas[key]['ideas'])))
+            else:
+                LEAFS.append("{}".format(ideas[key]['title']))
 
     def __str__(self):
         return self.title
@@ -24,7 +23,32 @@ class Node:
 if __name__ == '__main__':
     with open('1MPlayers.mup', 'r') as mindmap:
         root = json.load(mindmap)
-
         tree = Node(root['title'], root['ideas'])
-        for leaf in reversed(LEAFS):
-            print(leaf)
+
+        print('Goal / Why?: {}\n'.format(root['title']))
+
+        for node in reversed(LEAFS):
+            print(node)
+
+        '''
+        Example Layout:
+        ===
+
+        Goal / Why?: 1M Players
+
+            Actor / Who?: Players
+                Impact / How?: Inviting Friends
+                    Deliverables / What?:
+                        Viral Content
+                        Personalisation
+                        More Compelling Product
+                Impact / How?: Recommending
+                    Deliverables / What?:
+                        Bookmarking
+
+            Actor / Who?: Internal
+                Impact / How?: Organise PR Event
+                    Deliverables / What?:
+                        Invites
+                        Engaging out network
+        '''
